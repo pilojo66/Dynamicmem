12963568a3f30f9f72b266caec2d12ab8aac2f1b
pilojo66 <jp56469723@gmail.com>
2015-08-19 09:57:35 -0400

    Vectors and proper dynamic mem

--- main.cpp
+++ main.cpp
@@ -1,38 +1,76 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 using namespace std;
-
-
-void makeArray(int, int**);
-void fillArray(int, int*);
-void printArray(int, int*);
 
 int main() {
 	srand(time(NULL));
-	int a = 0;
-	int *b = nullptr;
-	cout << "How many items in the array?" << endl;
-	cin >> a;
-	makeArray(a, &b);
-	fillArray(a, b);
-	printArray(a, b);
-	system("PAUSE");
+	int *a = new int;
+	int *b = new int;
+
+	*a = 5;
+	*b = 7;
+	//DO STUFF WITH a AND b
+	delete a;
 	delete b;
-}
+	//DELETE TO SAVE MEMORY SPACE
 
-void makeArray(int size, int **d) {
-	*d = new int[size];
-}
-
-void fillArray(int size, int *d) {
-	for (int i = 0; i < size; i++) {
-		d[i] = rand() % 12315 + 1;
+	vector<int> v;
+	for (int i = 0; i < 56; i++) {
+		v.push_back(rand() % 100 + 1);
 	}
-}
-
-void printArray(int size, int *d) {
-	for (int i = 0; i < size; i++) {
-		cout << "Element " << i + 1 << " is: " << d[i] << ".\n";
+	for (vector<int>::iterator it = v.begin(); it != v.end();/*++ BEFORE iterator. VERY important */++it) {
+		cout << "The value is: " << *it << "." << endl;
 	}
+	v.clear();
+	cout << "The size of the vector is now: " << v.size() << endl;
+	
+
+
+	size_t s; //low-memory type for keeping a size
+	cout << "Enter a size of vector: " << endl;
+	cin >> s;
+	v.resize(s);
+	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+		*it = rand() % 512 + 1;
+	}
+
+
+
+	char *verify;
+	verify = new char;
+	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+		cout << "Is " << *it << " the correct value?(y/n)" << endl;
+		cin >> *verify;
+		if (*verify != 'y') {
+			cout << "Please choose a new value: ";
+			cin >> *it;
+		}
+	}
+
+	int *location;
+	int *value;
+	value = new int;
+	location = new int;
+	vector<int>::iterator locationIT;
+	cout << "Did you forget a value?(y/n)";
+	cin >> *verify;
+	if (*verify == 'y') {
+		cout << "Where would you like it to go?(last index is: " << v.size() << ")";
+		cin >> *location;
+		locationIT = v.begin() + *location - 1;
+		cout << "What is the value: ";
+		cin >> *value;
+		v.insert(locationIT, *value);
+	}
+	for (vector<int>::iterator it = v.begin(); it != v.end();/*++ BEFORE iterator. VERY important */++it) {
+		cout << "The value is: " << *it << "." << endl;
+	}
+	delete verify;
+	delete location;
+	delete value;
+	system("PAUSE");
+
+	return 0;
 }
